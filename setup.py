5e008f8c8cc1659d81857a67129e4c83806cf563
magda <magdalena.surowka@gmail.com>
2020-08-19 10:35:16 +0200

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="FirstOrderModel",
     version="1.0",
-    long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
